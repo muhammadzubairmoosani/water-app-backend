f64a6470dde3d560e6de3a35ab444c42ec1d2f06
Muhammad Zubair Moosani <muhammadzubairmoosani@gmail.com>
2020-10-06 13:57:12 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.port;
+const port = process.env.PORT;
 const mongodb = require("./src/config/config");
 const cors = require("cors");
 
