e29cb628eca0da7171426fbeb4d5d180e68584c7
muhammadzubairmoosani <muhammadzubairmoosani@gmail.com>
2020-11-15 23:23:27 +0500

    asdasd

--- src/routers/auth/index.js
+++ src/routers/auth/index.js
@@ -2,41 +2,64 @@ const express = require("express");
 const router = express.Router();
 const { supplierSchema } = require("../../schemas");
 const jwt = require("jsonwebtoken");
+const Cookies = require("universal-cookie");
 
 router.get("/supplier-login/:id", (req, res, next) => {
   // console.log(req.headers.cookie)
-  console.log('Cookies: ', req.headers)
+  // console.log("Cookies: ", req.cookies);
+
+  // const cookies = new Cookies(req.headers.cookie);
+
+  // console.log(cookies.get('myCat'));
+  console.log("== ", req.headers);
   // supplierSchema
-    // .findOne({ mobile: req.params.id })
-    // .then((data) => {
-    //   const token = jwt.sign(
-    //     { mobile: req.params.id },
-    //     process.env.REACT_APP_ACCESS_TOKEN_SECRET
-    //   );
-    //   res.send({ data, token });
-    // })
-    // .catch(next);
+  // .findOne({ mobile: req.params.id })
+  // .then((data) => {
+  //   const token = jwt.sign(
+  //     { mobile: req.params.id },
+  //     process.env.REACT_APP_ACCESS_TOKEN_SECRET
+  //   );
+  //   res.send({ data, token });
+  // })
+  // .catch(next);
 });
 
 router.post("/supplier-register", (req, res, next) => {
-  supplierSchema
-    .create(req.body)
-    .then((user) => {
-      const options = {
-        expiresIn: "1y",
-        audience: user._id,
-        role: "supplier",
-      };
-      const token = jwt.sign(
-        options,
-        process.env.REACT_APP_ACCESS_TOKEN_SECRET
-      );
-      res.send({
-        access_token: token,
-        refresh_token: process.env.REACT_APP_REFRESH_TOKEN_SECRET,
-      });
-    })
-    .catch(next);
+  res.cookie("key", "value").send()
+
+  //   supplierSchema
+  //     .create(req.body)
+  //     .then((user) => {
+  //       res
+  //     .writeHead(200, {
+  //       "Set-Cookie": "token=encryptedstring; HttpOnly",
+  //       "Access-Control-Allow-Credentials": "true"
+  //     })
+  //     .send();
+
+  //     // res.cookie("token", "Hello world").send("cookie set");
+  // // res.send()
+  //   })
+  //     .catch(next);
+
+  // supplierSchema
+  //   .create(req.body)
+  //   .then((user) => {
+  //     const options = {
+  //       expiresIn: "1y",
+  //       audience: user._id,
+  //       role: "supplier",
+  //     };
+  //     const token = jwt.sign(
+  //       options,
+  //       "process.env.REACT_APP_ACCESS_TOKEN_SECRET"
+  //     );
+  //     res.send({
+  //       access_token: token,
+  //       refresh_token: "process.env.REACT_APP_REFRESH_TOKEN_SECRET",
+  //     });
+  //   })
+  //   .catch(next);
 });
 
 // function anthenticateToken(req, res, next) {
