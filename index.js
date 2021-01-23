1fcac86053f73e435e6121b7db30e7f950302f4c
muhammadzubairmoosani <muhammadzubairmoosani@gmail.com>
2021-01-24 04:35:07 +0500

    ff

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 app.use((req, res, next) => {
   // res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://www.panivala.com");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With Content-Type, Accept, Authorization"
