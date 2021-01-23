ee133cd086f6cb99f38c0722d8037e6b9a5119d1
muhammadzubairmoosani <muhammadzubairmoosani@gmail.com>
2021-01-24 04:29:41 +0500

    kk

--- index.js
+++ index.js
@@ -8,10 +8,16 @@ const cookieSession = require("cookie-session");
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Credentials", true);
+  // res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE, PATCH");
+    return res.status(200).json({});
+  }
   next();
 });
 
