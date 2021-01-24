2449174789ebe481dc2f62824f6b7e58094644af
muhammadzubairmoosani <muhammadzubairmoosani@gmail.com>
2021-01-24 16:22:28 +0500

    lkj

--- index.js
+++ index.js
@@ -7,42 +7,48 @@ const cookieSession = require("cookie-session");
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-  // res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Origin", "https://www.panivala.com");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With Content-Type, Accept, Authorization"
-  );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE, PATCH");
-    return res.status(200).json({});
-  }
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
-var allowedOrigins = [
-  process.env.REACT_APP_LOCAL_HOST,
-  process.env.REACT_APP_ORIGIN,
-];
+// app.use((req, res, next) => {
+//   // res.header("Access-Control-Allow-Credentials", true);
+//   res.header("Access-Control-Allow-Origin", "https://www.panivala.com");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With Content-Type, Accept, Authorization"
+//   );
+//   if (req.method === "OPTIONS") {
+//     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE, PATCH");
+//     return res.status(200).json({});
+//   }
+//   next();
+// });
 
-app.use(
-  cors({
-    credentials: true,
-    origin: function (origin, callback) {
-      // allow requests with no origin
-      // (like mobile apps or curl requests)
-      // if(!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        var msg =
-          "The CORS policy for this site does not " +
-          "allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+// var allowedOrigins = [
+//   process.env.REACT_APP_LOCAL_HOST,
+//   process.env.REACT_APP_ORIGIN,
+// ];
+
+// app.use(
+//   cors({
+//     credentials: true,
+//     origin: function (origin, callback) {
+//       // allow requests with no origin
+//       // (like mobile apps or curl requests)
+//       // if(!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         var msg =
+//           "The CORS policy for this site does not " +
+//           "allow access from the specified Origin.";
+//         return callback(new Error(msg), false);
+//       }
+//       return callback(null, true);
+//     },
+//   })
+// );
 
 app.use(
   cookieSession({
