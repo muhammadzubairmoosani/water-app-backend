77b681f9be0e866e493327e97a613a74f1520d68
muhammadzubairmoosani <muhammadzubairmoosani@gmail.com>
2021-01-24 16:53:01 +0500

    sdf

--- index.js
+++ index.js
@@ -7,29 +7,45 @@ const cookieSession = require("cookie-session");
 
 app.use(express.json());
 
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "*");
+var allowedOrigins = [
+  process.env.REACT_APP_LOCAL_HOST,
+  process.env.REACT_APP_ORIGIN,
+];
 
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  // Pass to next layer of middleware
+function getCorsOptions(req, res, next) {
+  if (allowedOrigins.indexOf(req.headers.origin) > -1) {
+    res.set("Access-Control-Allow-Credentials", "true");
+    res.set("Access-Control-Allow-Origin", req.headers.origin);
+  } else {
+    // allow other origins to make unauthenticated CORS requests
+    res.set("Access-Control-Allow-Origin", "*");
+  }
   next();
-});
+}
+
+// app.use(function (req, res, next) {
+//   // Website you wish to allow to connect
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+
+//   // Request methods you wish to allow
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+//   );
+
+//   // Request headers you wish to allow
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "X-Requested-With,content-type"
+//   );
+
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader("Access-Control-Allow-Credentials", true);
+
+//   // Pass to next layer of middleware
+//   next();
+// });
 
 // app.use(function (req, res, next) {
 //   res.header("Access-Control-Allow-Credentials", true);
@@ -92,7 +108,7 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use("/", require("./src/routers"));
+app.use("/", getCorsOptions, require("./src/routers"));
 
 app.listen(process.env.REACT_APP_PORT, () =>
   console.log(`server is listing...`)
