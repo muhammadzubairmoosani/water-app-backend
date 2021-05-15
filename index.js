18af0400aad5027ca12e361aa45f91aa40d915c9
Muhammad Umair Moosani <muhammadumairmoosani@gmail.com>
2021-05-16 01:21:23 +0500

    CORS issue.

--- index.js
+++ index.js
@@ -5,37 +5,41 @@ const cors = require("cors");
 const passport = require("passport");
 const cookieSession = require("cookie-session");
 
+app.options("*", cors());
 app.use(express.json());
 
 var allowedOrigins = [
-  process.env.REACT_APP_LOCAL_HOST,
-  process.env.REACT_APP_ORIGIN,
+    process.env.REACT_APP_LOCAL_HOST,
+    process.env.REACT_APP_ORIGIN
 ];
 
-app.use(
-  cors({
-    credentials: true,
-    origin: function (origin, callback) {
-      if (allowedOrigins.indexOf(origin) === -1) {
-        var msg =
-          "The CORS policy for this site does not " +
-          "allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+// app.listen(4000, function () {
+//     console.log("CORS-enabled web server listening on port 4000");
+// });
+// app.use(
+//     cors({
+//         credentials: true,
+//         origin: function (origin, callback) {
+//             if (allowedOrigins.indexOf(origin) === -1) {
+//                 var msg =
+//                     "The CORS policy for this site does not " +
+//                     "allow access from the specified Origin.";
+//                 return callback(new Error(msg), false);
+//             }
+//             return callback(null, true);
+//         }
+//     })
+// );
 
 app.use(
-  cookieSession({
-    maxAge: 30 * 24 * 60 * 60 * 1000,
-    name: "session",
-    keys: [
-      process.env.REACT_APP_ACCESS_TOKEN_SECRET,
-      process.env.REACT_APP_REFRESH_TOKEN_SECRET,
-    ],
-  })
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        name: "session",
+        keys: [
+            process.env.REACT_APP_ACCESS_TOKEN_SECRET,
+            process.env.REACT_APP_REFRESH_TOKEN_SECRET
+        ]
+    })
 );
 
 app.use(passport.initialize());
@@ -44,6 +48,6 @@ app.use(passport.session());
 app.use("/", require("./src/routers"));
 
 app.listen(process.env.REACT_APP_PORT, () =>
-  console.log(`server is listing...`)
+    console.log(`server is listing...`)
 );
 mongodb.connection.once("open", () => console.log("database is connected!"));
