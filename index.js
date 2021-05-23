5746f80d7f342059958e06d8dd4829a34b5f55ab
muhammadzubairmoosani <muhammadzubairmoosani@gmail.com>
2021-05-24 00:56:27 +0500

    fix

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const cookieSession = require("cookie-session");
 // enable CORS without external module
 
 route.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://pani-vala-server.herokuapp.com/logged-in');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
