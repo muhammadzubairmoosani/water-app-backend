2d65792748429e1e703bb4e8d0392d599b1a31a1
muhammadzubairmoosani <muhammadzubairmoosani@gmail.com>
2021-05-24 00:55:02 +0500

    minor fix

--- index.js
+++ index.js
@@ -17,11 +17,30 @@ const cookieSession = require("cookie-session");
 // app.options('*', cors())
 
 // enable CORS without external module
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://pani-vala-server.herokuapp.com/logged-in");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+route.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
   next();
 });
+
+
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "https://pani-vala-server.herokuapp.com/logged-in");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 
 app.use(express.json());
