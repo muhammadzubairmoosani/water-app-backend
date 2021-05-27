1d2eb8380d6904629bfd7380a35390236fb9319e
muhammadzubairmoosani <muhammadzubairmoosani@gmail.com>
2021-05-28 00:45:29 +0500

    ll

--- src/routers/auth/index.js
+++ src/routers/auth/index.js
@@ -10,7 +10,22 @@ const passport = require("../../passport");
 // });
 
 router.get("/logged-in", (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  // res.header("Access-Control-Allow-Origin", "*");
+
+
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
+  
+  //  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+  
   if (req.user) return res.send(req.user);
   return res.send({ user: null });
 });
