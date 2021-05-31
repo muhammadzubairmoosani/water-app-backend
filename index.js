a5705230188b998f04b6e06bbb94b62c9ce30391
muhammadzubairmoosani <muhammadzubairmoosani@gmail.com>
2021-05-31 11:24:41 +0500

    a

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const cookieSession = require("cookie-session");
 
 app.use(cors());
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "https://www.panivala.com/");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
