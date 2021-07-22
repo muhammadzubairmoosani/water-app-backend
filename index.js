d7b4343a599efbdbd5c636de619c30e3bf93e14b
muhammadzubairmoosani <muhammadzubairmoosani@gmail.com>
2021-07-22 23:04:17 +0500

    minor fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongodb = require("./src/config/config");
 const cors = require("cors");
 const passport = require("passport");
 const cookieSession = require("cookie-session");
-const port = 'https://pani-vala-server.herokuapp.com/' || 4000
+const port =  4000
 app.use(express.json());
 
 
@@ -12,13 +12,6 @@ app.use(express.json());
 //   origin: '*'
 // }))
 
-app.use(
-  cors({
-    origin: process.env.REACT_APP_ORIGIN
-  })
-)
-
-console.log("REACT_APP_ORIGIN ===== ", process.env.REACT_APP_ORIGIN)
 
 // const allowedOrigins = [
 //   process.env.REACT_APP_LOCAL_HOST,
@@ -39,6 +32,14 @@ console.log("REACT_APP_ORIGIN ===== ", process.env.REACT_APP_ORIGIN)
 //     },
 //   })
 // );
+
+
+app.use(
+  cors({
+    credentials: true,
+    origin: 'http://localhost:3000'
+  })
+)
 
 app.use(
   cookieSession({
@@ -61,74 +62,3 @@ app.listen(port, () =>
 );
 
 mongodb.connection.once("open", () => console.log("database is connected!"));
-
-
-
-// ===============================
-// enable CORS without external module
-
-// app.use(function(req, res, next) {
-//   res.setHeader('Access-Control-Allow-Origin', 'https://pani-vala-server.herokuapp.com/logged-in');
-
-//   // Request methods you wish to allow
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-//   // Request headers you wish to allow
-//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
-
-//   // Set to true if you need the website to include cookies in the requests sent
-//   // to the API (e.g. in case you use sessions)
-//   res.setHeader('Access-Control-Allow-Credentials', true);
-
-//   // Pass to next layer of middleware
-//   next();
-// });
-
-
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "https://pani-vala-server.herokuapp.com/logged-in");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-
-
-
-// app.options("*", cors());
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-
-// app.use(cors());
-// app.options('*', cors())
-
-
-// var allowedOrigins = [
-//   process.env.REACT_APP_LOCAL_HOST,
-//   process.env.REACT_APP_ORIGIN
-// ];
-
-
-// app.use(
-//   cors({
-//     credentials: true,
-//     origin: true
-//   })
-// );
-
-
-// app.use(
-//   cors({
-//     credentials: true,
-//     origin: function (origin, callback) {
-//       if (allowedOrigins.indexOf(origin) === -1) {
-//         var msg =
-//           "The CORS policy for this site does not " +
-//           "allow access from the specified Origin.";
-//         return callback(new Error(msg), false);
-//       }
-//       return callback(null, true);
-//     }
-//   })
-// );
