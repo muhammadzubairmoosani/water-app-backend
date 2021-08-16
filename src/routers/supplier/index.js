800a3fbee796ffd0d6b282b612820c6adad0a866
muhammadzubairmoosani <muhammadzubairmoosani@gmail.com>
2021-08-16 23:13:33 +0500

    minor change

--- src/routers/supplier/index.js
+++ src/routers/supplier/index.js
@@ -5,12 +5,21 @@ const { User } = require("../../schemas");
 router.get("/suppliers/:skip/:limit/:key", (req, res, next) => {
 
   const { skip, limit, key } = req.params;
-  User.find(key !== 'null' ? { username: new RegExp(key, "i") } : {})
-  // User.find({})
-    .skip(parseInt(skip))
-    .limit(parseInt(limit))
-    .then(data => res.send(data))
-    .catch(next);
+
+  if (key !== 'null') {
+    User.find({ username: new RegExp(key, "i") })
+      .skip(parseInt(skip))
+      .limit(parseInt(limit))
+      .then(data => res.send(data))
+      .catch(next);
+
+  } else {
+    User.find({})
+      .skip(parseInt(skip))
+      .limit(parseInt(limit))
+      .then(data => res.send(data))
+      .catch(next);
+  }
 });
 
 router.get("/supplier-detail/:id", (req, res, next) => {
